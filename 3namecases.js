c71cd4932a940a6f853d8f21bf2a246d29c37800
Sheraz0Ahmed <160455097+Sheraz0Ahmed@users.noreply.github.com>
2024-03-04 10:19:14 +0500

    Add files via upload

---
+++ 3namecases.js
@@ -0,0 +1,12 @@
+"use strict";
+// store person name in a variable
+// for Lowercase
+let name1 = "FAZIL";
+console.log("Lowercase:" + name1.toLowerCase());
+// for Uppercase
+let name3 = "fazil";
+console.log("Uppercase:" + name3.toUpperCase());
+// for Titlecase
+let name2 = "fazil";
+let titlecase = name1.charAt(0).toUpperCase() + name2.slice(1);
+console.log("Tiltecase:" + titlecase);
---
+++ 3namecases.ts
@@ -0,0 +1,16 @@
+// store person name in a variable
+
+// for Lowercase
+let name1:string = "FAZIL"
+console.log("Lowercase:" +name1.toLowerCase())
+
+// for Uppercase
+let name3:string = "fazil"
+console.log("Uppercase:" +name3.toUpperCase())
+
+// for Titlecase
+let name2:string = "fazil"
+let titlecase = name1.charAt(0).toUpperCase() + name2.slice(1);
+console.log("Tiltecase:"+ titlecase)
+
+
